5024d613cc0fa656ab80424f98b5cf3d8a7f01c8
mahmoud noby <mahmoud.noby@nabdacare.com>
2021-09-19 01:33:38 +0200

    logout

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB_XbFw3-CXCrdjcyNR3yZodLoRdLmGyGw",
+    authDomain: "school-tops.firebaseapp.com",
+    projectId: "school-tops",
+    storageBucket: "school-tops.appspot.com",
+    messagingSenderId: "737072289915",
+    appId: "1:737072289915:web:d47676167535ffe4f9aade"
+  }
 };
